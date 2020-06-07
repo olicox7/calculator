07d4afd538faf6cac307cd65035bb18c7c48d702
Oliver Cox <olicox7@gmail.com>
2020-06-07 18:46:33 +0100

    operators to functions and display with event listeners

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const display = document.querySelector("#calculatorScreen");
-const numberButtons = document.querySelectorAll(".numberButton");
 let displayValue
-numberButtons.forEach(button => button.addEventListener("click",() => {
-    display.value += button.textContent
-    displayValue = +display.value
-}));
+
+let currentOperator
+
+let result
 
 const operate = (operator,num1,num2) => {
     if(operator === "+") return add(num1,num2);
@@ -14,6 +12,18 @@ const operate = (operator,num1,num2) => {
     return "Invalid operator";
 }
 
+const storeVals = function() {
+    storedNum = displayValue
+    display.value = ""
+    storedOperator = this.textContent
+}
+
+const equalsOperation = () => {
+    result = operate(storedOperator, storedNum, displayValue);
+    display.value = result;
+    displayValue = result;
+}
+
 const add = (num1,num2) => num1 + num2;
 
 const subtract = (num1,num2) => num1 - num2;
@@ -21,3 +31,25 @@ const subtract = (num1,num2) => num1 - num2;
 const multiply = (num1,num2) => num1 * num2;
 
 const divide = (num1,num2) => num1 / num2;
+
+const display = document.querySelector("#calculatorScreen");
+
+const numberButtons = document  .querySelectorAll(".numberButton");
+
+const operatorButtons = document .querySelectorAll(".operatorButton");
+
+const numberToDisplay = function() {
+    if (display.value == result) {
+        display.value = "";
+    }
+    display.value += this.textContent;
+    displayValue = +display.value;
+}
+
+const equalsButton = document.querySelector("#equalsButton");
+
+numberButtons.forEach(button => button.addEventListener("click", numberToDisplay));
+
+operatorButtons.forEach(button => button.addEventListener("click", storeVals));
+
+equalsButton.addEventListener("click",equalsOperation);
