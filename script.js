830257062bc95266586837b4fe9d2d6fb34d5b59
Oliver Cox <olicox7@gmail.com>
2020-06-07 01:33:10 +0100

    number buttons now update the display

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const display = document.querySelector("#calculatorScreen");
+const numberButtons = document.querySelectorAll(".numberButton");
+let displayValue
+numberButtons.forEach(button => button.addEventListener("click",() => {
+    display.value += button.textContent
+    displayValue = +display.value
+}));
+
 const operate = (operator,num1,num2) => {
-    if(operator === "+") return add(num1,num2)
-    if(operator === "-") return subtract(num1,num2)
-    if(operator === "÷") return divide(num1,num2)
-    if(operator === "×") return multiply(num1,num2)
-    return "Invalid operator"
+    if(operator === "+") return add(num1,num2);
+    if(operator === "-") return subtract(num1,num2);
+    if(operator === "÷") return divide(num1,num2);
+    if(operator === "×") return multiply(num1,num2);
+    return "Invalid operator";
 }
 
 const add = (num1,num2) => num1 + num2;
